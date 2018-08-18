ce86b52afe6246196859614fe853f5b2c3ed72f8
chelss093@gmail.com <chelss093@gmail.com>
2018-08-18 18:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  
+  return('Happy Holidays, ${name}!')
 }
+
+function happyHolidayTo(holiday, name) {
+  return('Happy ${holiday}, ${name}!')
+}
+
